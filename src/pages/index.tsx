b798ed33fe6ea97961cf19e03023feeee0f8b630
Davi Serrano <daviscardoso2901@gmail.com>
2022-05-02 21:28:31 -0300

    Styled component configured

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
+import { GlobalStyle } from '../style/global'
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>NetxAPP</h1>
-      
+      <GlobalStyle />
+
     </div>
   )
 }
---
+++ src/style/global.ts
@@ -0,0 +1,81 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        --background: #ffffff;
+        
+        --blue-ligth: #6933FF;
+         
+        --blue: #5429CC;
+        --green: #33CC95;
+        --black: #141414;
+        --red: #E52E4D;
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+        --shape: #ffffff;
+    }
+  *{
+       margin: 0;
+       padding: 0;
+       box-sizing: border-box;
+   }
+   html{
+       @media (max-width: 1180px){
+           font-size: 93.75%;
+       }
+       @media (max-width: 720px){
+           font-size: 87.5%;
+       }
+       
+    }
+    body, button, input{
+        font-family: 'Roboto', sans-serif;
+        background: none;
+        border: none;
+    } 
+    
+   h1, h2, h3, h4, h4, h6{
+    font-weight: 600;
+   }
+   body{
+       background: var(--background);
+       -webkit-font-smoothing: antialiased;
+   }
+   button{
+       cursor: pointer;
+   }
+   [disabled] {
+       opacity: 0.6;
+       cursor: not not-allowed;
+   }
+   .react-modal-overplay{
+        background: rgba(0, 0, 0, 0.5);
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+   }
+   .react-modal-content{
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.25rem;
+   }
+   .react-modal-close{
+       position: absolute;
+       right: 1.5rem;
+       top:1.5rem;
+       border: 0;
+       background: transparent;
+       transition: 0.2s;
+       &:hover{
+           filter: brightness(0.8);
+       }
+   }
+`
